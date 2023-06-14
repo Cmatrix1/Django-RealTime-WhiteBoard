ee7e5a5cb45c1c4bb821e94af99d9b5f39592a86
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-14 17:22:08 +0330

    react code for create Excalidraw board in html

---
+++ front/script.js
@@ -0,0 +1,17 @@
+const App = () => {
+    return React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(
+            "div",
+            {
+                style: { height: "100%" },
+            },
+            React.createElement(ExcalidrawLib.Excalidraw)
+        )
+    );
+};
+
+const excalidrawWrapper = document.getElementById("app");
+const root = ReactDOM.createRoot(excalidrawWrapper);
+root.render(React.createElement(App));
