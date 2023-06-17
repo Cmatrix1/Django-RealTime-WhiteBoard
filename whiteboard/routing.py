893ec5143b5a2a8983ef713f5070c365c2839d80
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-17 22:47:55 +0330

    add messages Backend Cnsumer

--- whiteboard/routing.py
+++ whiteboard/routing.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from whiteboard import consumers
 
 websocket_urlpatterns = [
     path('ws/board/', consumers.BoardConsumer.as_asgi()),
+    re_path(r'ws/conference/(?P<slug>[\w-]+)/$', consumers.ConferenceRoomConsumer.as_asgi()),
 ]
