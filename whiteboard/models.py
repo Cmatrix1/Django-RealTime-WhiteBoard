4fde415cca93b1f344f89b057bf6dbb2ede1af3a
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-17 22:28:56 +0330

    add some field for type of confrence and make better the relations

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -7,12 +7,21 @@ class ConferenceRoom(models.Model):
     name = models.CharField(max_length=255)
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='my_rooms')
     participants = models.ManyToManyField(User, through='Participant')
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+    allow_anonymous = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+    
+    def is_participant(self, user: User) -> bool:
+        if self.allow_anonymous or self.admin == user:
+            return True
+        return self.participants.filter(id=user.id).exists()
 
 
 class Participant(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
@@ -22,10 +31,10 @@ class Participant(models.Model):
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Participant, on_delete=models.CASCADE, null=True, blank=True)
+    room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE, related_name="messages")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} - {}: {}".format(self.sender.username, self.room.name, self.content[:50])
+        return "{} - {}: {}".format(self.sender.name, self.room.name, self.content[:50])
