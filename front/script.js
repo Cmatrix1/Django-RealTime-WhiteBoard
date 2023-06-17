443137e6cc01859832478d99daf25c320b01cb4b
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-17 23:16:52 +0330

    js method for save the image  from canvas and send it to backend bot not test it yet

--- front/script.js
+++ front/script.js
@@ -20,3 +20,26 @@ root.render(React.createElement(App));
 
 // Buttons Events
 $(".leave-btn").click(() => window.location = "/" );
+
+
+// Method For Send The Board To Backend
+function saveCanvasImage(canvas) {
+    canvas.toBlob(function (blob) {
+      var formData = new FormData();
+      formData.append('image', blob, 'canvas_image.png');
+  
+      $.ajax({
+        url: '/api//image/',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+          console.log('Image saved:', data.file_path);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.error(errorThrown);
+        }
+      });
+    });
+  }
