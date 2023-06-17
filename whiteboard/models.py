fb72c095d880d04f472f16c7406a593a1acb19d8
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-17 17:41:24 +0330

    add models for message

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class ConferenceRoom(models.Model):
+    name = models.CharField(max_length=255)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='my_rooms')
+    participants = models.ManyToManyField(User, through='Participant')
+
+    def __str__(self):
+        return self.name
+
+
+class Participant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.user.username, self.room.name)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}: {}".format(self.sender.username, self.room.name, self.content[:50])
