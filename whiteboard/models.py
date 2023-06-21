b5122fa8a98cc61523175468d6e6bf225f2c217d
Cmatrix1 <sadeghmajidi980@gmail.com>
2023-06-21 12:16:23 +0330

    update the models and session

--- whiteboard/models.py
+++ whiteboard/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.sessions.models import Session
 
 
 class ConferenceRoom(models.Model):
@@ -24,6 +24,7 @@ class Participant(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
